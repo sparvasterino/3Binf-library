cf9a8e19f463f4f1756fd7072a0ef2c731070f03
Your Name <you@example.com>
2019-10-12 09:50:38 +0200

    add init function

---
+++ main.py
@@ -0,0 +1,9 @@
+class Book:
+    def  _init_(self, title, author, sbn):
+         self.title: title
+         self.author = author
+         self.sbn = sbn
+
+harry_potter = Book("Harry Potter e il calice di fuoco", "J.K.Rowling",1)
+print(harry_potter.title)
+
